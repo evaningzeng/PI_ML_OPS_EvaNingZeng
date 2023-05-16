aa6d89085fe81ee0030e879a3f0b97f87fbac751
evaningzeng <105259101+evaningzeng@users.noreply.github.com>
2023-05-16 01:20:38 -0300

    Streamlit App

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import requests
+
+# URL de tu backend FastAPI en render.com
+BASE_URL = "https://pi-ml-ops-evaningzeng.onrender.com"
+
+# Función para realizar la solicitud al endpoint de recomendación
+def obtener_recomendacion(titulo):
+    url = f"{BASE_URL}/recomendacion/{titulo}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("recomendaciones", [])
+    return []
+
+# Interfaz de usuario con Streamlit
+def main():
+    st.title("Sistema de Recomendación de Películas")
+
+    # Formulario para ingresar el título de la película
+    titulo = st.text_input("Ingrese el título de la película")
+
+    # Botón para obtener las recomendaciones
+    if st.button("Obtener Recomendaciones"):
+        if titulo:
+            recomendaciones = obtener_recomendacion(titulo)
+            if recomendaciones:
+                st.subheader("Recomendaciones:")
+                for i, recomendacion in enumerate(recomendaciones):
+                    st.write(f"{i+1}. {recomendacion}")
+            else:
+                st.write("No se encontraron recomendaciones.")
+        else:
+            st.write("Ingrese el título de la película.")
+
+if __name__ == "__main__":
+    main()
